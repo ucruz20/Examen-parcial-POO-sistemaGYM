290841e508541986dc72b57911d22ec65e8876aa
Ulises Cruz <ulisescrz.16@gmail.com>
2025-09-28 14:36:43 -0600

    base

---
+++ src/main/java/org/groupfive/gymapi/GymApiApplication.java
@@ -0,0 +1,12 @@
+package org.groupfive.gymapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymApiApplication.class, args);
+    }
+}
