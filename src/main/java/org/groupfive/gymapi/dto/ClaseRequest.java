370d9ff2e2102eefe18b7d627a7bc6bae8ca993e
KennetCastro <CS24010@ues.edu.sv>
2025-11-05 08:42:06 -0600

    Añade dependencia de validacion y uso de jakarta

--- src/main/java/org/groupfive/gymapi/dto/ClaseRequest.java
+++ src/main/java/org/groupfive/gymapi/dto/ClaseRequest.java
@@ -1,5 +1,6 @@
 package org.groupfive.gymapi.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClaseRequest {
+    @NotBlank
     private String nombre;
+
+    @NotNull
+    @Min(1)
     private int cupoMaximo;
+
+    @NotBlank
     private String horario;
+
+    @NotNull
     private Long entrenadorId;
 }
--- src/main/java/org/groupfive/gymapi/model/Clase.java
+++ src/main/java/org/groupfive/gymapi/model/Clase.java
@@ -2,18 +2,38 @@ package org.groupfive.gymapi.model;
 
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Clase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_clase")
     private Long id;
+
+    @Column(nullable = false)
     private String nombre;
+
+    @Column(name = "cupo_max", nullable = false)
     private int cupoMaximo;
+
+    @Column(name = "horario", nullable = false)
     private String horario;
+    
+    @ManyToOne(optional = false)
+    @Column(name = "id_entrenador")
     private Long entrenadorId;
+
     private List<Long> inscritos;
 }
